88d01158a1ec7d42a69079f88e5889d2005254d4
muhammad.faisal <muhammad.faisal@u-blox.com>
2021-09-07 16:40:53 +0500

    subsequent string index has been added

---
+++ problem solving/RemoveDuplicateFromString.cs
@@ -0,0 +1,52 @@
+﻿//// C# program to remove duplicate character
+//// from character array and print in sorted
+//// order
+////Time Complexity : O(n* n)
+//using System;
+//using System.Collections.Generic;
+//class GFG
+//{
+//    static String removeDuplicate(char[] str, int n)
+//    {
+//        // Used as index in the modified string
+//        int index = 0;
+//        //geeksforgeeks
+//        // Traverse through all characters
+//        for (int i = 0; i < n; i++)
+//        {
+
+//            // Check if str[i] is present before it
+//            int j;
+//            for (j = 0; j <= i; j++)
+//            {
+//                if (str[i] == str[j])
+//                {
+//                    break;
+//                }
+//            }
+
+//            // If not present, then add it to
+//            // result.
+//            //geeksforgeeks
+//            if (j == i)
+//            {
+
+//                str[index++] = str[i];
+//            }
+//        }
+//        char[] ans = new char[index];
+//        Array.Copy(str, ans, index);
+//        return String.Join("", ans);
+//    }
+
+//    // Driver code
+//    public static void Main(String[] args)
+//    {
+//        char[] str = "geeksforgeeks".ToCharArray();
+//        int n = str.Length;
+//        Console.WriteLine(removeDuplicate(str, n));
+//        Console.ReadLine();
+//    }
+//}
+
+//// This code is contributed by PrinciRaj1992
---
+++ problem solving/numberexchange.cs
@@ -0,0 +1,37 @@
+﻿//// C# Program to add two numbers
+//// without using arithmetic operator
+//using System;
+
+//class GFG
+//{
+//    static int Add(int x, int y)
+//    {
+//        // Iterate till there is no carry
+//        while (y != 0)
+//        {
+//            // carry now contains common
+//            // set bits of x and y
+//            int carry = x & y;
+
+//            // Sum of bits of x and
+//            // y where at least one
+//            // of the bits is not set
+//            x = x ^ y;
+
+//            // Carry is shifted by
+//            // one so that adding it
+//            // to x gives the required sum
+//            y = carry << 1;
+//        }
+//        return x;
+//    }
+
+//    // Driver code
+//    public static void Main()
+//    {
+//        Console.WriteLine(Add(15, 32));
+//        Console.ReadLine();
+//    }
+//}
+
+//// This code is contributed by vt_m.

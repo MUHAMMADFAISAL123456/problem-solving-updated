bce7d3cfc797e766d6785a9adb745d3c0f5ac730
muhammad.faisal <muhammad.faisal@u-blox.com>
2021-09-13 11:29:38 +0500

    subsequent string has been added

---
+++ problem solving/RefOut.cs
@@ -0,0 +1,28 @@
+﻿//// C# program to check if a string is
+//// substring of other.
+//using System;
+//class SubsequentString
+//{
+
+//    public static void Main()
+//    {
+//        int i = 1;
+//        Console.WriteLine("Previous value of integer i:" + i.ToString());
+//        string test = GetNextName(i);
+//        Console.WriteLine("Current value of integer i:" + i.ToString());
+//        Console.ReadLine();
+//    }
+//    public static string GetNextName(int id)
+//    {
+//        string returnText = "Next-" + id.ToString();
+//        id += 1;
+//        return returnText;
+//    }
+//    public static string GetNextName(ref int id)
+//    {
+//        string returnText = "Next-" + id.ToString();
+//        id += 1;
+//        return returnText;
+//    }
+//}
+
--- problem solving/SubsequentString.cs
+++ problem solving/SubsequentString.cs
@@ -1,48 +1,48 @@
-﻿// C# program to check if a string is
-// substring of other.
-using System;
-class SubsequentString
-{
+﻿//// C# program to check if a string is
+//// substring of other.
+//using System;
+//class SubsequentString
+//{
 
-    // Returns true if s1 is substring of s2
-    static int isSubstring(string s1, string s2)
-    {
-        int M = s1.Length;
-        int N = s2.Length;
+//    // Returns true if s1 is substring of s2
+//    static int isSubstring(string s1, string s2)
+//    {
+//        int M = s1.Length;
+//        int N = s2.Length;
 
-        /* A loop to slide pat[] one by one */
-        for (int i = 0; i <= N - M; i++)
-        {
-            int j;
+//        /* A loop to slide pat[] one by one */
+//        for (int i = 0; i <= N - M; i++)
+//        {
+//            int j;
 
-            /* For current index i, check for
-			pattern match */
-            for (j = 0; j < M; j++)
-                if (s2[i + j] != s1[j])
-                    break;
+//            /* For current index i, check for
+//			pattern match */
+//            for (j = 0; j < M; j++)
+//                if (s2[i + j] != s1[j])
+//                    break;
 
-            if (j == M)
-                return i;
-        }
+//            if (j == M)
+//                return i;
+//        }
 
-        return -1;
-    }
+//        return -1;
+//    }
 
-    /* Driver code */
-    public static void Main()
-    {
-        string s1 = "ks";
-        string s2 = "geeksforgeeks";
+//    /* Driver code */
+//    public static void Main()
+//    {
+//        string s1 = "ks";
+//        string s2 = "geeksforgeeks";
 
-        int res = isSubstring(s1, s2);
+//        int res = isSubstring(s1, s2);
 
-        if (res == -1)
-            Console.Write("Not present");
-        else
-            Console.Write("Present at index "
-                        + res);
-        Console.ReadLine();
-    }
-}
+//        if (res == -1)
+//            Console.Write("Not present");
+//        else
+//            Console.Write("Present at index "
+//                        + res);
+//        Console.ReadLine();
+//    }
+//}
 
-// This code is contributed by nitin mittal.
+//// This code is contributed by nitin mittal.

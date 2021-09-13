0cf28d9f9fb351c680f6605bdca7b66608d11de6
muhammad.faisal <muhammad.faisal@u-blox.com>
2021-09-13 11:30:46 +0500

    add second highest number

--- problem solving/SecondHighestNumber.cs
+++ problem solving/SecondHighestNumber.cs
@@ -1,35 +1,35 @@
-﻿// C# program to check if a string is
-// substring of other.
-using System;
-class SubsequentString
-{
+﻿//// C# program to check if a string is
+//// substring of other.
+//using System;
+//class SubsequentString
+//{
 
-    public static void Main()
-    {
-        int max = 0;
-        int secondmax = 0;
-        int[] arr = { 2, 11, 15, 1, 7, 99, 6, 85, 4 };
+//    public static void Main()
+//    {
+//        int max = 0;
+//        int secondmax = 0;
+//        int[] arr = { 2, 11, 15, 1, 7, 99, 6, 85, 4 };
 
-        for (int r = 0; r < arr.Length; r++)
-        {
-            if (max < arr[r])
-            {
-                max = arr[r];
-            }
-        }
+//        for (int r = 0; r < arr.Length; r++)
+//        {
+//            if (max < arr[r])
+//            {
+//                max = arr[r];
+//            }
+//        }
 
-        for (int r = 0; r < arr.Length; r++)
-        {
-            if (secondmax < arr[r] && arr[r] < max)
-            {
-                secondmax = arr[r];
-            }
-        }
+//        for (int r = 0; r < arr.Length; r++)
+//        {
+//            if (secondmax < arr[r] && arr[r] < max)
+//            {
+//                secondmax = arr[r];
+//            }
+//        }
 
-        Console.WriteLine(max);
-        Console.WriteLine(secondmax);
-        Console.Read();
-    }
+//        Console.WriteLine(max);
+//        Console.WriteLine(secondmax);
+//        Console.Read();
+//    }
  
-}
+//}
 
